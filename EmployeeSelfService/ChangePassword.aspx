f1904b69948edd7ef2c120587fe79af13417c20a
Scott Odle <scott@sjodle.com>
2016-04-03 05:30:25 -0700

    Change Password view

---
+++ EmployeeSelfService/ChangePassword.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="EmployeeSelfService.ChangePassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Change Password</h1>
+        <div>
+            Current Password:&nbsp;&nbsp;&nbsp;<asp:TextBox ID="CurrentPass" runat="server" TextMode="Password"></asp:TextBox><br />
+            New Password:&nbsp;&nbsp;&nbsp;<asp:TextBox ID="NewPass" runat="server" TextMode="Password"></asp:TextBox><br />
+            Confirm Password:&nbsp;&nbsp;&nbsp;<asp:TextBox ID="ConfPass" runat="server" TextMode="Password"></asp:TextBox><br />
+            <asp:Button ID="SubmitButton" runat="server" Text="Change Password" OnClick="SubmitButton_Click" /><br />
+            <asp:Label ID="ErrorText" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmployeeSelfService/ChangePassword.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeeSelfService
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] == null)
+            {
+                Response.BufferOutput = true;
+                Response.Redirect("/Login.aspx?nextPage=" + Request.ServerVariables["URL"]);
+            }
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            if (NewPass.Text != ConfPass.Text)
+            {
+                ErrorText.Text = "New passwords don't match!";
+                return;
+            }
+
+            try
+            {
+                ESSLogin.ChangePassword(Session["Username"].ToString(), CurrentPass.Text, NewPass.Text);
+                Response.BufferOutput = true;
+                Response.Redirect("/SessionTest.aspx");
+            } catch (InvalidLoginException)
+            {
+                ErrorText.Text = "Your username and password do not match any of our records.";
+                return;
+            }
+        }
+    }
+}
